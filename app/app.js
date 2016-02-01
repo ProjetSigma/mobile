5210cb3888d87d8561b6361c00b2caa5aecfe3f9
Basile Bruneau <basilebruneau@gmail.com>
2016-02-01 15:56:26 +0100

    Missing rxjs

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ import {App, Platform} from 'ionic/ionic';
 import {LoginPage} from './pages/login/login';
 import {AuthService} from './services/auth-service';
 import {RestService} from './services/rest-service';
+import 'rxjs/Rx';
 
 
 @App({
